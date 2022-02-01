7a8e73a0eadc944c4469b4bc12e9c6d9f4e452ee
ryantruong25 <75468904+ryan-truong@users.noreply.github.com>
2022-01-31 22:21:12 -0800

    added method, test and lib

--- SkillsDemo1.java
+++ SkillsDemo1.java
@@ -1,1 +1,5 @@
-class SkillsDemo1{}
+public class SkillsDemo1{
+    public static int subtract(int a, int b){
+        return a - b;
+    }
+}
---
+++ SkillsDemo1Test.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillsDemo1Test {
+    @Test
+    public void subtractTest(){
+        assertEquals(1,SkillsDemo1.subtract(5,1));
+    }
+}
